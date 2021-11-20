735583f7d1981e876b2802f09fdbe3e8208f6229
ifyour <ifyour@outlook.com>
2021-11-20 16:43:21 +0800

    theme config

--- theme.config.js
+++ theme.config.js
@@ -1,21 +1,19 @@
 const YEAR = new Date().getFullYear();
+const COPYRIGHT_STATEMENT = 'This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.';
 
 export default {
+  head: (
+    <>
+      <link rel="alternate" type="application/rss+xml" title="RSS" href="/feed.xml" />
+      <link rel="preload" href="/fonts/Inter-roman.latin.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+    </>
+  ),
   footer: (
     <small style={{ display: "block", marginTop: "8rem" }}>
-      <abbr
-        title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
-        style={{ cursor: "help" }}
-      >
-        CC BY-NC 4.0
-      </abbr>{" "}
+      <abbr title={COPYRIGHT_STATEMENT} style={{ cursor: "help" }} >CC BY-NC 4.0</abbr>
+      {' '}
       <time>{YEAR}</time> © Ming's blog
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-      `}</style>
+      <a style={{ float: 'right' }} href="/feed.xml">RSS</a>
     </small>
   ),
 };
